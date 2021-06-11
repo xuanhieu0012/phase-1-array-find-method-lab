327f2ce82cb2ef6b2a405670bd161fba70723ccc
ddeleon267 <dominique.a.deleon@gmail.com>
2021-06-11 16:39:09 -0400

    commit tree 20eb00d7f14efb3e94c2ea64fe7c1ba57096b5f1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+// const record = [
+//     {year: "2017", result: "W"}
+// ]
+
+function superbowlWin(record){
+   const ex = record.find(r => {
+       console.log(r.result)
+       if (r.result === "W"){
+        return r.year
+       }
+   })
+   return ex.year
+
+}
 
