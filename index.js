35bfad513600e1642c8da6fb269c4ded5e9524c9
Hieu <xuanhieu0012@gmail.com>
2021-08-11 11:31:23 -0500

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+// find the year winning supperbowl
+// const = record=[]
+
+
+function superbowlWin(record){
+  const winningYear = record.find(ele => ele.result === "W")
+  
+  return winningYear ? winningYear.year : undefined
+
+}
+superbowlWin(record);
 
